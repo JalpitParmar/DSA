fe89280225a8eadc288b1807d66b9db7869e4e8f
rajdeepsinh7979 <rajdeepsinhj494@gmail.com>
2026-04-26 22:42:16 +0530

    second part for ans5

--- day0/rajdeepsinh/solution 5.py
+++ day0/rajdeepsinh/solution 5.py
@@ -12,3 +12,18 @@ for i in range(10,1,-1):
         if(i%2==0):
             print("* ",end="")
     print()
+
+
+# for i in range(1,12):
+#     print(" "*(12-i),end="")
+#     if(i%2!=0):
+#         print("* "*i,end="")
+#     else:
+#         print()
+# print()
+# for i in range(10,1,-1):
+#     print(" "*(12-i+1),end="")
+#     if(i%2==0):
+#         print("* "*(i-1),end="")
+#     else:
+#         print()
